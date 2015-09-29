bd400964a419403c6fc9b7660c360e5f5553fd7c
James Louie Puro <james.louie.puro@gmail.com>
2015-09-30 00:58:23 +0800

    Add new config.js as angular env

---
+++ resources/assets/js/config.js
@@ -0,0 +1,7 @@
+var configData = {
+    basePath: '/',
+    apiBaseUrl: 'http://localhost:8000',
+    apiTokenName: 'cortejando',
+    loginPath: '/login',
+    tokenPath: '/mock/accessToken.json'
+}
