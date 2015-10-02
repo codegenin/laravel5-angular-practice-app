abf25f6748a4346b1113f9c36c13137f94c19666
James Louie Puro <james.louie.puro@gmail.com>
2015-10-03 00:21:09 +0800

    Refactor seeder to use factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -14,6 +14,10 @@
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
+        'description' => $faker->sentence(mt_rand(3, 5)),
+        'dob'   => $faker->date('Y-m-d'),
+        'phone' => $faker->phoneNumber,
+        'gender' => $faker->randomElement(['male','female']),
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,16 +12,6 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = [
-            'name'          => 'James Puro',
-            'description'   => 'I am J',
-            'dob'           => '1983-05-01',
-            'phone'         => '+12-123-1234',
-            'gender'        => 'male',
-            'email'         => 'james@test.com',
-            'password'      => Hash::make('123')
-        ];
-
-        User::create($users);
+        factory(App\User::class, 20)->create();
     }
 }
