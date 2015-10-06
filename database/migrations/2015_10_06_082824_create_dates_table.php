431a1741c88afe2996c494df01ab39c4c65e1aca
James Louie Puro <james.louie.puro@gmail.com>
2015-10-06 16:41:50 +0800

    Add dates table migration

---
+++ database/migrations/2015_10_06_082824_create_dates_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('description');
+            $table->time('time');
+            $table->string('place');
+            $table->string('location_name');
+            $table->integer('location_lat');
+            $table->integer('location_long');
+            $table->enum('state', ['active', 'pending', 'completed']);
+            $table->integer('owner_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('dates');
+    }
+}
